175d508bc77fe9553e98541f2d96e78c8202d793
ProdOrDev <mwmarrano@gmail.com>
2024-02-07 15:12:17 -0500

    Fully (i think?) implement all the instructions.

--- src/tms1100/mod.rs
+++ src/tms1100/mod.rs
@@ -6,4 +6,331 @@
 //! - Data Manual: <http://www.bitsavers.org/components/ti/TMS1000/TMS_1000_Series_Data_Manual_Dec76.pdf>
 //! - Programmers Reference: <https://en.wikichip.org/w/images/f/ff/TMS1000_Series_Programmer%27s_reference_manual.pdf>
 
+pub mod mem;
 pub mod pla;
+
+use arbitrary_int::{u1, u11, u3, u4, u5, u6, u7, Number};
+
+use mem::{Ram, Rom};
+use pla::{Entry, Fixed};
+
+/// An emulated TMS1100 micro-processor.
+#[derive(Default, Debug, Clone)]
+pub struct Tms1100 {
+    /// The K\[1,2,4,8\] pin inputs.
+    pub k: u4,
+    /// The R\[0-10\] pin outputs.
+    pub r: u11,
+    /// The O\[0-7\] pin outputs.
+    ///
+    /// ## Note
+    ///
+    /// An important thing to note here is that this is not actually
+    /// the 8-bit O value, instead this the un-PLAed value of the O
+    /// pins. To make use of this value it must be put through your
+    /// custom PLA first.
+    pub o: u5,
+
+    /// The accumulator A.
+    pub a: u4,
+    /// The address register X.
+    pub x: u3,
+    /// The address register Y.
+    pub y: u4,
+    /// The program counter PC.
+    pub pc: u6,
+    /// The subroutine return register SR.
+    ///
+    /// This stores the previous program counter.
+    pub sr: u6,
+    /// The page address register PA.
+    pub pa: u4,
+    /// The page buffer register PB.
+    pub pb: u4,
+    /// The chapter address latch CA.
+    ///
+    /// This stores the current chapter data.
+    pub ca: u1,
+    /// The chapter buffer latch CB.
+    ///
+    /// This stores the succeeding chapter data and transfers to the CA pending
+    /// the successful execution of a subsequent branch or call instruction.
+    pub cb: u1,
+    /// The chapter subroutine latch CS.
+    ///
+    /// This stores the return address after successfully executing a call instruction
+    /// (CA -> CS). CS transfers data back to CA when the return from subroutine (RETN)
+    /// instruction occurs.
+    pub cs: u1,
+    /// The call latch C.
+    pub call_latch: bool,
+    /// The status latch S.
+    ///
+    /// This stores the result from adder operations/comparisons and can be used to
+    /// conditionally branch.
+    pub status_latch: bool,
+
+    /// A sub-instruction cycle counter.
+    pub cycle: Counter,
+    /// The current opcode.
+    pub opcode: u8,
+    /// The fixed instruction of the current opcode.
+    pub fixed: Fixed,
+    /// The micro-instructions of the current opcode.
+    pub micro: Entry,
+    /// The CKI data bus.
+    ///
+    /// The contents of this bus vary depending on the currently executing opcode.
+    pub cki_bus: u4,
+    /// The lower 4-bit constant of the current opcode.
+    c: u4,
+    /// A value read from, or to be written to, RAM.
+    ram_data: u4,
+    /// The internal ALU (or adder).
+    pub alu: Alu,
+
+    /// The onboard (2048 x 8bit) Read Only Memory (ROM) chip.
+    pub rom: Rom,
+    /// The onboard (128 x 4bit) Random Access Memory (RAM) chip.
+    pub ram: Ram,
+}
+
+impl Tms1100 {
+    /// Create a new TMS1100 micro-processor.
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Execute a single sub-instruction cycle.
+    pub fn clock(&mut self) {
+        #[allow(clippy::match_same_arms)]
+        match self.cycle {
+            Counter::Cycle0 => {
+                // Execute the second part of the branch/call/return fixed instructions.
+                self.fixed_br();
+                self.fixed_call();
+                self.fixed_retn();
+
+                // Read the constant or K input for the current opcode.
+                self.read_cki_bus();
+
+                // Read data from RAM.
+                self.ram_data = self.ram.read();
+
+                // Clear the inputs and outputs of the ALU.
+                self.alu.reset();
+            }
+            Counter::Cycle1 => {
+                // Update the ROM address.
+                self.rom.addr =
+                    u11::from(self.ca) << 10 | u11::from(self.pa) << 6 | u11::from(self.pc);
+
+                // Update the N input of the ALU.
+                self.op_ftn();
+                self.op_atn();
+                self.op_natn();
+                self.op_ckn();
+                self.op_mtn();
+
+                // Update the P inout of the ALU.
+                self.op_ckp();
+                self.op_mtp();
+                self.op_ytp();
+
+                // Update the carry input of the ALU.
+                self.op_cin();
+            }
+            Counter::Cycle2 => {
+                // Perform the ALU logic.
+                self.alu.clock();
+
+                // Update the ALU status.
+                self.op_c8();
+                self.op_ne();
+                self.op_ckm();
+
+                // Perform the rest of the fixed instructions and decide
+                // which value to write back to RAM.
+                self.op_sto();
+
+                self.fixed_sbit();
+                self.fixed_rbit();
+                self.fixed_setr();
+                self.fixed_rstr();
+                self.fixed_tdo();
+                self.fixed_ldx();
+                self.fixed_comx();
+                self.fixed_ldp();
+                self.fixed_comc();
+
+                // Write the (potentially modified) RAM data back to RAM.
+                self.ram.write(self.ram_data);
+            }
+            Counter::Cycle3 => {}
+            Counter::Cycle4 => {
+                // Store any potential ALU outputs into registers.
+                self.op_auta();
+                self.op_auty();
+                self.op_stsl();
+
+                // Read the next opcode.
+                self.read_opcode();
+
+                // Update the RAM address.
+                self.ram.addr = u7::new(self.x.value() << 4 | self.y.value());
+            }
+            Counter::Cycle5 => {}
+        }
+
+        self.cycle.next();
+    }
+
+    /// Read the correct value for the current opcode onto the CKI data bus.
+    fn read_cki_bus(&mut self) {
+        self.cki_bus = match self.opcode & 0xf8 {
+            // Opcode: 00001XXX, reads the K inputs.
+            0x08 => self.k,
+            // Opcode: 0011XXXX, select the bit to modify.
+            0x30 | 0x38 => u4::new(1) << ((self.c.value() >> 2) ^ 0xf),
+            // Opcode: 01XXXXXX, a constant value.
+            0x00 | 0x40 | 0x48 | 0x50 | 0x58 | 0x60 | 0x68 | 0x70 | 0x78 => self.c,
+            _ => u4::new(0),
+        }
+    }
+
+    /// Increment the program counter.
+    fn next_pc(&mut self) {
+        // The program counter is Linear Feedback Shift Register (LFSR).
+        //
+        // This means that a feedback bit exists which is a XOR of the
+        // highest two bits. However, this bit does make an exception
+        // when all the low bits of the program counter are set.
+
+        let mut feedback = (self.pc << 1) >> 5 & self.pc >> 5;
+
+        if self.pc == u6::MAX >> 1 {
+            feedback = u6::new(1);
+        } else if self.pc == u6::MAX {
+            feedback = u6::new(0);
+        }
+
+        self.pc = self.pc << 1 | feedback;
+    }
+
+    /// Read the opcode at the current program counter, then increment
+    /// the program counter.
+    fn read_opcode(&mut self) {
+        let op = self.rom.read();
+        self.opcode = op;
+
+        // The lower 4-bits of the opcode is a constant value,
+        // however most instructions expect this to be bitswapped.
+        self.c = u4::new(op & 0xf).reverse_bits();
+
+        self.fixed = Fixed::decode(op);
+        self.micro = Entry::decode(op);
+
+        self.next_pc();
+    }
+}
+
+/// A sub-instruction cycle counter.
+///
+/// The TMS1100 operates on 6 oscillator cycles within a larger machine cycle,
+/// with the general process going: fetch data from memory, then execute an
+/// operation.
+#[derive(Default, Debug, Clone, Copy)]
+pub enum Counter {
+    /// The first sub-instruction cycle.
+    ///
+    /// The second part of the branch/call/return instructions are executed,
+    /// additionally the K inputs are checked, data is read from RAM, and
+    /// the inputs to the ALU are cleared.
+    #[default]
+    Cycle0,
+    /// The second sub-instruction cycle.
+    ///
+    /// The inputs to the ALU are supplied with their corresponding data.
+    Cycle1,
+    /// The third sub-instruction cycle.
+    ///
+    /// The ALU performs its specified logic operation, the remaining fixed
+    /// instructions are executed, and data is written back to RAM.
+    Cycle2,
+    /// The fourth sub-instruction cycle.
+    ///
+    /// The first part of the register store operations are executed.
+    Cycle3,
+    /// The fifth sub-instruction cycle.
+    ///
+    /// The second part of the register store operations are executed.
+    Cycle4,
+    /// The sixth sub-instruction cycle.
+    ///
+    /// The next instruction is decoded and the first part of the branch/call/return
+    /// instruction are executed.
+    Cycle5,
+}
+
+impl Counter {
+    /// Go to the next sub-instruction cycle.
+    pub fn next(&mut self) {
+        *self = match self {
+            Self::Cycle0 => Self::Cycle1,
+            Self::Cycle1 => Self::Cycle2,
+            Self::Cycle2 => Self::Cycle3,
+            Self::Cycle3 => Self::Cycle4,
+            Self::Cycle4 => Self::Cycle5,
+            Self::Cycle5 => Self::Cycle0,
+        }
+    }
+}
+
+/// The internal ALU (or adder) of the TMS1100.
+#[derive(Default, Debug, Clone)]
+pub struct Alu {
+    /// The P (lhs) input of the ALU.
+    pub p: u4,
+    /// The N (rhs) input of the ALU.
+    pub n: u4,
+    /// The result output of the ALU.
+    pub res: u4,
+
+    /// The CIN (carry) input of the ALU.
+    pub carry_in: bool,
+    /// The COUT (carry) output of the ALU.
+    pub carry_out: bool,
+
+    /// The status output of the ALU.
+    pub status: bool,
+}
+
+impl Alu {
+    /// Create a new ALU for the TMS1100.
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Reset the current state of the ALU.
+    pub fn reset(&mut self) {
+        *self = Self::default();
+    }
+
+    /// Clock the ALU.
+    ///
+    /// This performs an add (with optional carry) operation.
+    pub fn clock(&mut self) {
+        let carry_in = u4::from(u8::from(self.carry_in));
+
+        let (res, c1) = self.p.overflowing_add(self.n);
+        let (res, c2) = res.overflowing_add(carry_in);
+
+        self.carry_out = c1 || c2;
+        self.res = res;
+
+        // This will be modified by micro-instructions.
+        self.status = true;
+    }
+}
--- src/tms1100/pla.rs
+++ src/tms1100/pla.rs
@@ -1,5 +1,7 @@
 //! Emulation of the Programmable Logic Array (PLA) used for instruction decoding.
 
+use arbitrary_int::{u1, u11, u3, u4, u5, u6, Number};
+
 bitfield::bitfield! {
     /// An entry in the instruction decode PLA.
     ///
@@ -7,7 +9,7 @@ bitfield::bitfield! {
     /// opcode/instruction. However, not every instruction uses the PLA
     /// for execution, some opcodes are decoded using a fixed (non-programmable)
     /// logic scheme ([Fixed]).
-    #[derive(Clone, Copy)]
+    #[derive(Default, Clone, Copy)]
     pub struct Entry(u16);
     impl Debug;
     /// The micro-instruction CTP.
@@ -153,7 +155,7 @@ impl Entry {
 /// are useless, in fact activating certain micro-instructions in the
 /// PLA for these fixed instructions can enable completely new instructions
 /// to be formed.
-#[derive(Debug, Clone, Copy)]
+#[derive(Default, Debug, Clone, Copy)]
 pub enum Fixed {
     /// A branch instruction.
     Br,
@@ -201,6 +203,7 @@ pub enum Fixed {
     ///
     /// This is returned from [Fixed::decode] if the given opcode is not
     /// one of the 12 fixed instructions.
+    #[default]
     None,
 }
 
@@ -222,6 +225,225 @@ impl Fixed {
             0x80..=0xaf => Self::Br,
             0xc0..=0xff => Self::Call,
             _ => Self::None,
+        }
+    }
+}
+
+impl super::Tms1100 {
+    /// Execute the fixed instruction BR.
+    pub(super) fn fixed_br(&mut self) {
+        if matches!(self.fixed, Fixed::Br) && self.status_latch {
+            if !self.call_latch {
+                self.pa = self.pb;
+            }
+
+            self.ca = self.cb;
+            self.pc = u6::new(self.opcode & 0x3f);
+        }
+    }
+
+    /// Execute the fixed instruction CALL.
+    pub(super) fn fixed_call(&mut self) {
+        if matches!(self.fixed, Fixed::Call) && self.status_latch {
+            let prev_pa = self.pa;
+
+            if !self.call_latch {
+                self.call_latch = true;
+                self.sr = self.pc;
+                self.pa = self.pb;
+                self.cs = self.ca;
+            }
+
+            self.ca = self.cb;
+            self.pb = prev_pa;
+            self.pc = u6::new(self.opcode & 0x3f);
+        }
+    }
+
+    /// Execute the fixed instruction RETN.
+    pub(super) fn fixed_retn(&mut self) {
+        if matches!(self.fixed, Fixed::Retn) {
+            if self.call_latch {
+                self.call_latch = false;
+                self.pc = self.sr;
+                self.ca = self.cs;
+            }
+
+            self.pa = self.pb;
+        }
+    }
+
+    /// Execute the fixed instruction SBIT.
+    pub(super) fn fixed_sbit(&mut self) {
+        if matches!(self.fixed, Fixed::Sbit) {
+            self.ram_data |= self.cki_bus ^ u4::new(0xf);
+        }
+    }
+
+    /// Execute the fixed instruction RBIT.
+    pub(super) fn fixed_rbit(&mut self) {
+        if matches!(self.fixed, Fixed::Rbit) {
+            self.ram_data &= self.cki_bus;
+        }
+    }
+
+    /// Execute the fixed instruction SETR.
+    pub(super) fn fixed_setr(&mut self) {
+        if matches!(self.fixed, Fixed::Setr) {
+            let idx = (self.x.value() >> 2) << 4 | self.y.value();
+            self.r |= u11::new(1) << idx;
+        }
+    }
+
+    /// Execute the fixed instruction RSTR.
+    pub(super) fn fixed_rstr(&mut self) {
+        if matches!(self.fixed, Fixed::Rstr) {
+            let idx = (self.x.value() >> 2) << 4 | self.y.value();
+            self.r &= !(u11::new(1) << idx);
+        }
+    }
+
+    /// Execute the fixed instruction TDO.
+    pub(super) fn fixed_tdo(&mut self) {
+        if matches!(self.fixed, Fixed::Tdo) {
+            self.o = u5::new(u8::from(self.status_latch)) | u5::new(self.a.value());
+        }
+    }
+
+    /// Execute the fixed instruction LDX.
+    pub(super) fn fixed_ldx(&mut self) {
+        if matches!(self.fixed, Fixed::Ldx) {
+            self.x = u3::new(self.c.value() >> 1);
+        }
+    }
+
+    /// Execute the fixed instruction COMX.
+    pub(super) fn fixed_comx(&mut self) {
+        if matches!(self.fixed, Fixed::Comx) {
+            self.x ^= u3::MAX;
+        }
+    }
+
+    /// Execute the fixed instruction LDP.
+    pub(super) fn fixed_ldp(&mut self) {
+        if matches!(self.fixed, Fixed::Ldp) {
+            self.pb = self.c;
+        }
+    }
+
+    /// Execute the fixed instruction COMC.
+    pub(super) fn fixed_comc(&mut self) {
+        if matches!(self.fixed, Fixed::Comc) {
+            self.cb ^= u1::MAX;
+        }
+    }
+}
+
+impl super::Tms1100 {
+    /// Execute the micro-instruction 15TN.
+    pub(super) fn op_ftn(&mut self) {
+        if self.micro.ftn() {
+            self.alu.n |= u4::MAX;
+        }
+    }
+
+    /// Execute the micro-instruction ATN.
+    pub(super) fn op_atn(&mut self) {
+        if self.micro.atn() {
+            self.alu.n |= self.a;
+        }
+    }
+    /// Execute the micro-instruction NATN.
+    pub(super) fn op_natn(&mut self) {
+        if self.micro.natn() {
+            self.alu.n |= !self.a;
+        }
+    }
+    /// Execute the micro-instruction CKN.
+    pub(super) fn op_ckn(&mut self) {
+        if self.micro.ckn() {
+            self.alu.n |= self.cki_bus;
+        }
+    }
+    /// Execute the micro-instruction MTN.
+    pub(super) fn op_mtn(&mut self) {
+        if self.micro.mtn() {
+            self.alu.n |= self.ram_data;
+        }
+    }
+
+    /// Execute the micro-instruction CKP.
+    pub(super) fn op_ckp(&mut self) {
+        if self.micro.ckp() {
+            self.alu.p |= self.cki_bus;
+        }
+    }
+    /// Execute the micro-instruction MTP.
+    pub(super) fn op_mtp(&mut self) {
+        if self.micro.mtp() {
+            self.alu.p |= self.ram_data;
+        }
+    }
+    /// Execute the micro-instruction YTP.
+    pub(super) fn op_ytp(&mut self) {
+        if self.micro.ytp() {
+            self.alu.p |= self.y;
+        }
+    }
+
+    /// Execute the micro-instruction CIN.
+    pub(super) fn op_cin(&mut self) {
+        if self.micro.cin() {
+            self.alu.carry_in = true;
+        }
+    }
+
+    /// Execute the micro-instruction C8.
+    pub(super) fn op_c8(&mut self) {
+        if self.micro.c8() {
+            self.alu.status &= self.alu.carry_out;
+        }
+    }
+
+    /// Execute the micro-instruction NE.
+    pub(super) fn op_ne(&mut self) {
+        if self.micro.ne() {
+            self.alu.status &= self.alu.p != self.alu.n;
+        }
+    }
+
+    /// Execute the micro-instruction CKM.
+    pub(super) fn op_ckm(&mut self) {
+        if self.micro.ckm() {
+            self.ram_data = self.cki_bus;
+        }
+    }
+
+    /// Execute the micro-instruction STO.
+    pub(super) fn op_sto(&mut self) {
+        if self.micro.sto() {
+            self.ram_data = self.a;
+        }
+    }
+
+    /// Execute the micro-instruction AUTA.
+    pub(super) fn op_auta(&mut self) {
+        if self.micro.auta() {
+            self.a = self.alu.res;
+        }
+    }
+
+    /// Execute the micro-instruction AUTY.
+    pub(super) fn op_auty(&mut self) {
+        if self.micro.auty() {
+            self.y = self.alu.res;
+        }
+    }
+
+    /// Execute the micro-instruction STSL.
+    pub(super) fn op_stsl(&mut self) {
+        if self.micro.stsl() {
+            self.status_latch = self.alu.status;
         }
     }
 }
