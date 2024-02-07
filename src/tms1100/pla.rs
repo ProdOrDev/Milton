fa6141048b6f7ec72641e5a442b4c2d605a0e5e5
ProdOrDev <mwmarrano@gmail.com>
2024-02-06 23:23:26 -0500

    Implement fixed instructions and add register/memory transfers.

--- src/tms1100/pla.rs
+++ src/tms1100/pla.rs
@@ -39,7 +39,8 @@ bitfield::bitfield! {
     /// of the adder.
     ///
     /// This micro-instruction is slightly odd because it exists on the chip,
-    /// however none of the PLA combinations actually make use of it.
+    /// however none of the PLA combinations actually make use of it. My best
+    /// thought is that is was reserved for future revisions of the processor.
     pub mtn,  _:  5;
     /// The micro-instruction 15TN.
     ///
@@ -121,6 +122,14 @@ impl Entry {
             0x07 => Self(MTP | FTN | C8 | AUTA),
             0x08 => Self(CKP | AUTA),
             0x0e => Self(CKP | NE),
+            0x20 => Self(ATN | AUTY),
+            0x21 => Self(MTP | AUTA),
+            0x22 => Self(MTP | AUTY),
+            0x23 => Self(YTP | AUTA),
+            0x24 => Self(STO | YTP | FTN | C8 | AUTY),
+            0x25 => Self(STO | YTP | CIN | C8 | AUTY),
+            0x26 => Self(STO | AUTA),
+            0x27 => Self(STO),
             0x38..=0x3b => Self(CKP | CKN | MTP | NE),
             0x3c => Self(MTP | NATN | CIN | C8 | AUTA),
             0x3d => Self(NATN | CIN | C8 | AUTA),
@@ -132,6 +141,87 @@ impl Entry {
             0x70..=0x7e => Self(CKP | ATN | CIN | C8 | AUTA),
             0x7f => Self(CKP | CIN | C8 | AUTA),
             _ => Self(0),
+        }
+    }
+}
+
+/// A set of 12 fixed instructions.
+///
+/// Unlike most instructions with can be modified via the PLA, certain
+/// instructions like branch/call have fixed (non-programmable) logic
+/// decoders. However, this does not mean the PLA slots for these opcodes
+/// are useless, in fact activating certain micro-instructions in the
+/// PLA for these fixed instructions can enable completely new instructions
+/// to be formed.
+#[derive(Debug, Clone, Copy)]
+pub enum Fixed {
+    /// A branch instruction.
+    Br,
+    /// A subroutine call instruction.
+    Call,
+    /// A complement instruction.
+    ///
+    /// This complements (inverts) the chapter buffer.
+    Comc,
+    /// A complement instruction.
+    ///
+    /// This complements (inverts) the X register.
+    Comx,
+    /// A load instruction.
+    ///
+    /// This loads the page buffer with a constant value.
+    Ldp,
+    /// A load instruction.
+    ///
+    /// This loads the X register with a file address.
+    Ldx,
+    /// A bit instruction.
+    ///
+    /// This resets the specified bit in RAM.
+    Rbit,
+    /// A subroutine return instruction.
+    Retn,
+    /// An output instruction.
+    ///
+    /// This resets the 11-bit output R addressed by the Y register.
+    Rstr,
+    /// A bit instruction.
+    ///
+    /// This sets the specified bit in RAM.
+    Sbit,
+    /// An output instruction.
+    ///
+    /// This sets the 11-bit output R addressed by the Y register.
+    Setr,
+    /// A transfer instruction.
+    ///
+    /// This transfers data from the accumulator and status latch into the O output.
+    Tdo,
+    /// The current opcode is not a fixed instruction.
+    ///
+    /// This is returned from [Fixed::decode] if the given opcode is not
+    /// one of the 12 fixed instructions.
+    None,
+}
+
+impl Fixed {
+    /// Decode a TMS1100 fixed instruction from the given opcode.
+    #[must_use]
+    pub fn decode(op: u8) -> Self {
+        match op {
+            0x09 => Self::Comx,
+            0x0a => Self::Tdo,
+            0x0b => Self::Comc,
+            0x0c => Self::Rstr,
+            0x0d => Self::Setr,
+            0x0f => Self::Retn,
+            0x30..=0x33 => Self::Sbit,
+            0x34..=0x37 => Self::Rbit,
+            0x01..=0x1f => Self::Ldp,
+            0x28..=0x2f => Self::Ldx,
+            0x80..=0xaf => Self::Br,
+            0xc0..=0xff => Self::Call,
+            _ => Self::None,
         }
     }
 }
